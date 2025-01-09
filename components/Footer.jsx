e8a5fd5e14c7af9548f599d8223d5c6c1cb00f2f
majid-the-dev <contactmajidoladepo@gmail.com>
2025-01-09 10:34:30 +0100

    Fix: footer date

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
   return (
     <div className="bg-black flex flex-col lg:flex-row items-center justify-between gap-4 px-4 lg:px-10 pb-10">
       <p className="text-gray-200 text-sm" ref={footerRef1}>
-        All Rights Reserved &copy; 2024 Harmony Group NG
+        All Rights Reserved &copy; 2025 Harmony Group NG
       </p>
       <div
         className="flex items-center gap-5 text-gray-200 text-sm"
